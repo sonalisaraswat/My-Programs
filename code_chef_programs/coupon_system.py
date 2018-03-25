6da3083a013fe6665d2e9ca6d5a2683814e48773
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2018-03-25 19:17:33 +0530

    Update coupon_system.py

--- code_chef_programs/coupon_system.py
+++ code_chef_programs/coupon_system.py
@@ -1,30 +1,25 @@
 for t in range(int(input())):
-    d1=0
-    d2=0
-    d3=0
-    c1=0
-    c2=0
-    c3=0
+    d1 = d2 = d3 = c1 = c2 = c3 = 0
     for n in range(int(input())):
-        c,l,d=[int(j) for j in input().split()]
-        if l==1:
-            if d>d1:
-                d1=d
-                c1=c
-            if d==d1 and c<c1:
-                c1=c
-        if l==2:
-             if d>d2:
-                 d2=d
-                 c2=c
+        c, l, d = [int(j) for j in input().split()]
+        if l == 1:
+            if d > d1:
+                d1 = d
+                c1 = c
+            if d == d1 and c < c1:
+                c1 = c
+        if l == 2:
+             if d > d2:
+                 d2 = d
+                 c2 = c
              if d == d2 and c < c2:
                  c2 = c
-        if l==3:
-            if d>d3:
-                d3=d
-                c3=c
-            if d==d3 and c<c3:
-                c3=c
-    print(d1,c1)
-    print(d2,c2)
+        if l == 3:
+            if d > d3:
+                d3 = d
+                c3 = c
+            if d == d3 and c < c3:
+                c3 = c
+    print(d1, c1)
+    print(d2, c2)
     print(d3,c3)

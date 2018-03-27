79e6e11c1a1741e7245ce7d9b0a8a3ed9426558d
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2018-03-27 14:27:12 +0530

    Add files via upload

---
+++ code_chef_programs/J_reached_safetly_or_not.py
@@ -0,0 +1,22 @@
+for t in range(int(input())):
+    m,n = [int(j) for j in input().split()]
+    rx,ry = [int(k) for k in input().split()]
+    num=int(input())
+    s=input()
+    x=0
+    y=0
+    for i in range(num):
+        if s[i]=='L':
+            x-=1
+        if s[i]=='R':
+            x+=1
+        if s[i]=='U':
+            y+=1
+        if s[i]=='D':
+            y-=1
+    if x==rx and y==ry:
+        print("Case %d: REACHED"%(t+1))
+    elif x<0 or x>m or y<0 or y>n:
+        print("Case %d: DANGER"%(t+1))
+    else:
+        print("Case %d: SOMEWHERE"%(t+1))

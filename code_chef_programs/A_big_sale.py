b3386d5ad01c3a76ed4cffcf28e5022481de36a2
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2018-03-29 12:42:44 +0530

    Update A_big_sale.py

--- code_chef_programs/A_big_sale.py
+++ code_chef_programs/A_big_sale.py
@@ -1,8 +1,7 @@
 for t in range(int(input())):
     loss=0
     for n in range(int(input())):
-        p1=0
-        p2=0
+        p1=p2=0
         p,q,d=[int(j) for j in (input().split())]
         p1=p+(d*p/100)
         p2=p1-(d*p1/100)
